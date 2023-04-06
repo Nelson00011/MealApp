abfadab32ff749499164ba3dbeee7f1fda7bf3bf
Ayla Nelson <aylanelson01@gmail.com>
2023-04-05 20:54:37 -0700

    outline README.md TODO header

---
+++ food/src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+const Header = props => {
+    return (
+        <Fragment>
+        <header>
+        TODO header
+        </header>
+        <div></div>
+        </Fragment>
+    )
+};
+
+export default Header;
