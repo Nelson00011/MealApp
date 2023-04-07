ec65844fbe7a79a60c18555d0660d9680cbbaf71
Ayla Nelson <aylanelson01@gmail.com>
2023-04-07 09:30:11 -0700

    cart buttons backdrop  close TODO cart context

--- food/src/components/Layout/Header.js
+++ food/src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>Meal App</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
          <div className={classes['main-image']}>
         <img src={sushi} alt='A table full of delicious seafood sushi'/>
